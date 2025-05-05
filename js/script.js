397779ff7fac73c7668672b00e3cb65fda4a4e8e
Francehws <arenafrancescovito02@gmail.com>
2025-05-05 15:21:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+    document.getElementById('contactPopup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('contactPopup').style.display = 'none';
+}
